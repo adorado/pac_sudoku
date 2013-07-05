7b4fa4f909ff3f2e8428a4c0283b8546dc9647ed
adorado <arieldorado@gmail.com>
2013-07-05 18:19:31 -0400

    new files

---
+++ io.py
@@ -0,0 +1,120 @@
+
+import csv
+from validmatrix import MatrixHandler
+from config import Configfile
+from xml.dom import minidom
+
+class FileHandler:
+	
+	def __init__(self, file, mode='r'):
+		self.file_name = file
+		try:
+			self.file = open(self.file_name, mode)
+		except:
+			self.file_name = file.split('.')[0] + '_new.' + file.split('.')[1]
+			self.file = open(self.file_name, 'w')
+			self.reopen(mode)
+	
+	def reopen(self, mode):
+		if not self.file.closed:
+			self.file.close()
+		self.file = open(self.file_name, mode)
+
+class FileHandlerTXT(FileHandler):
+	def __init__(self, file, mode='r'):
+		FileHandler.__init__(self, file, mode)
+		
+	def export_file(self, matrix):
+		if 'r' in self.file.mode:
+			raise IOError("File not open for writing")
+		else:
+			for row in matrix:
+				line = ''
+				for digit in row:
+					line += str(digit)
+				line += '\n'
+				self.file.write(line)
+			self.file.close()
+
+	def not_empty(self):
+		#file_to_import = open(self.file, 'r')
+		file_imported = self.file.read()
+		if file_imported != "":
+			return True
+		else:
+			return False
+		self.file.close()
+		
+	def import_file(self):
+		#file_to_import = open(self.file, 'r')
+		file_imported = self.file.readlines()
+		linematrix = []
+		matrix = []
+		self.file.close()
+		for line in file_imported:
+			linematrix.append(line.rstrip())
+		for i in  range(len(linematrix)):
+			row = linematrix[i]
+			rowint=[]
+			for j in range(len(row)):
+				if row[j] in ['a','b','c','d','e','f','g','h','a','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','S','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']:
+					pass
+			else:
+				rowint.append(int(row[j]))
+			matrix.append(rowint)
+		return matrix
+	
+class FileHandlerXML(FileHandler):
+	def __init__(self, file, mode='r'):
+		FileHandler.__init__(self, file, mode)
+		if 'r' in self.file.mode:
+			self.xmldoc = minidom.parse(self.file)
+	
+	def parseconfig(self):
+		if 'w' in self.file.mode:
+			raise IOError("File not open for reading")
+		inputType = str(self.xmldoc.\
+			getElementsByTagName('inputType')[0].childNodes[0].nodeValue)
+		outputType = str(self.xmldoc.\
+			getElementsByTagName('outputType')[0].childNodes[0].nodeValue)
+		defaultAlgorithm = str(self.xmldoc.\
+			getElementsByTagName('defaultAlgorithm')[0].childNodes[0].nodeValue)
+		difficultyLevel = str(self.xmldoc.\
+			getElementsByTagName('difficultyLevel')[0].childNodes[0].nodeValue)
+		config = Configfile(inputType, outputType,\
+			defaultAlgorithm, difficultyLevel)
+		return config
+
+	def create_config_file(self, config):
+		if 'r' in self.file.mode:
+			raise IOError("File not open for writing")
+		self.file.write("<config>")
+		self.file.write("    <inputType>"+ config.inputType+"</inputType>")
+		self.file.write("    <outputType>"+ config.outputType+"</outputType>")
+		self.file.write("    <defaultAlgorithm>"+ config.defaultAlgorithm+"</defaultAlgorithm>")
+		self.file.write("    <difficultyLevel>"+ config.difficultyLevel+"</difficultyLevel>")
+		self.file.write("</config>")
+		self.file.close()  
+
+class FileHandlerCSV(FileHandler):
+	def import_file(self):
+		selfmatrix=[]
+		selfmatrixaux=[]
+		with self.file as f:
+			reader = csv.reader(f)
+			if reader!="":
+				for row in reader:
+					for a in range(len(row)):
+						selfmatrixaux.append(int(row[a]))
+					selfmatrix.append(selfmatrixaux)
+					selfmatrixaux=[]
+		mat=MatrixHandler(selfmatrix)
+		if mat.validate()==True:
+			return selfmatrix
+		else:
+			return False
+
+
+		
+	
+	
---
+++ test_io.py
@@ -0,0 +1,161 @@
+import unittest
+import csv
+from validmatrix import MatrixHandler
+from io import FileHandler, FileHandlerXML, FileHandlerTXT,FileHandlerCSV
+from config import Configfile
+
+class TestFileHandlerTXT(unittest.TestCase):
+ 	def setUp(self):
+ 		self.matrix = [[4, 1, 7, 3, 6, 9, 8, 2, 5],\
+ 						[6, 3, 2, 1, 5, 8, 9, 4, 7],\
+ 						[9, 5, 8, 7, 2, 4, 3, 1, 6],\
+ 						[8, 2, 5, 4, 3, 7, 1, 6, 9],\
+ 						[7, 9, 1, 5, 8, 6, 4, 3, 2],\
+ 						[3, 4, 6, 9, 1, 2, 7, 5, 8],\
+ 						[2, 8, 9, 6, 4, 3, 5, 7, 1],\
+ 						[5, 7, 3, 2, 9, 1, 6, 8, 4],\
+ 						[1, 6, 4, 8, 7, 5, 2, 9, 3]]
+ 		
+ 		with open("export_expected.txt") as file:
+ 			self.txt_content_expected = file.readlines()
+ 		
+ 		self.file_actual = "export_actual.txt"
+ 		self.file_import = FileHandlerTXT("sudoku_import.txt",'r')
+ 	
+ 	def test_export_matrix_to_txt(self):
+ 		txthandler = FileHandlerTXT(self.file_actual,'w')
+ 		txthandler.export_file(self.matrix)
+ 		with open(self.file_actual) as file:
+ 			txt_content_actual = file.readlines()
+ 		
+ 		self.assertEqual(self.txt_content_expected,txt_content_actual)
+ 
+ 	def test_export_txt_and_close(self):
+ 		txthandler = FileHandlerTXT(self.file_actual, 'w')
+ 		txthandler.export_file(self.matrix)
+ 		self.assertTrue(txthandler.file.closed)
+ 		
+ 	def test_exporting_to_txt_file_in_read_mode(self):
+ 		txthandler = FileHandlerTXT(self.file_actual)
+ 		self.assertRaises(IOError, txthandler.export_file, self.matrix)
+ 		
+ 	#-------------------------------------------
+ 	
+		
+	def test_file_to_import_is_not_empty(self):
+		notemptyfile = self.file_import.not_empty()
+		self.assertEqual(True, notemptyfile)
+		
+	def test_file_imported_will_create_a_list_with_9_elements(self):
+		listlenght = len(self.file_import.import_file())
+		self.assertEqual(9, listlenght)
+
+class TestFileHandlerXML(unittest.TestCase):
+	def setUp(self):
+		self.expected_tuple = ("CSV", "Console", "Peter Norvig", "Medium")
+		self.expected_config = Configfile()
+		self.custom_config = Configfile(*self.expected_tuple)
+		self.file_expected = "config_expected.xml"
+		self.file_actual = "config_actual.xml"
+		with open(self.file_expected) as rawfile:
+			self.expected_xml_content = rawfile.readlines()
+
+ 	def test_parse_config_in_write_mode(self):
+ 		xmlhandler = FileHandlerXML(self.file_actual, 'w')
+ 		self.assertRaises(IOError,	xmlhandler.parseconfig)
+	
+	def test_config_instance_is_created(self):
+		xmlhandler = FileHandlerXML(self.file_expected)
+		actual_config = xmlhandler.parseconfig()
+		self.assertEqual(self.expected_tuple,\
+			(actual_config.inputType, actual_config.outputType,\
+			actual_config.defaultAlgorithm, actual_config.difficultyLevel))
+		
+	def test_config_file_is_created(self):
+		xmlhandler = FileHandlerXML(self.file_actual,'w')
+		xmlhandler.create_config_file(self.custom_config)
+		with open(self.file_actual) as rawfile:
+			actual_xml_content = rawfile.readlines()
+		self.assertEqual(actual_xml_content, self.expected_xml_content)
+
+ 	def test_save_config_and_close(self):
+ 		xmlhandler = FileHandlerXML(self.file_actual, 'w')
+ 		xmlhandler.create_config_file(self.custom_config)
+ 		self.assertTrue(xmlhandler.file.closed)
+ 		
+ 	def test_save_config_in_read_mode(self):
+ 		xmlhandler = FileHandlerXML(self.file_actual)
+ 		self.assertRaises(IOError,\
+			xmlhandler.create_config_file, self.custom_config)
+
+class TestFileHandler(unittest.TestCase):
+	def setUp(self):
+		self.file_valid = "valid_file.txt"
+		with open(self.file_valid, 'w') as file:
+			file.write('')
+		
+		self.file_invalid = "invalid_file.txt"
+		self.file_invalid_expected = "invalid_file_new.txt"
+		self.expected_mode = "r"
+	
+	def test_open_valid_file(self):
+		actual_handler = FileHandler(self.file_valid)
+		self.assertEqual(self.file_valid, actual_handler.file.name)
+	
+	def test_open_invalid_file(self):
+		actual_handler = FileHandler(self.file_invalid)
+		self.assertEqual(self.file_invalid_expected, actual_handler.file.name)
+	
+	def test_reopening(self):
+		handler = FileHandler(self.file_valid, 'w')
+		handler.reopen(self.expected_mode)
+		actual_mode = handler.file.mode
+		self.assertEqual(self.expected_mode, actual_mode)
+
+class TestFileHandlerCSV(unittest.TestCase):
+	def setUp(self):
+		self.file_valid = "valid_file.csv"
+		with open(self.file_valid, 'w') as file1:			
+			file1.write('4,0,0,0,0,0,8,0,5\n')
+			file1.write('0,3,0,0,0,0,0,0,0\n')
+			file1.write('0,0,0,7,0,0,0,0,0\n')
+			file1.write('0,2,0,0,0,0,0,6,0\n')
+			file1.write('0,0,0,0,8,0,4,0,0\n')
+			file1.write('0,0,0,0,1,0,0,0,0\n')
+			file1.write('0,0,0,6,0,3,0,7,0\n')
+			file1.write('5,0,0,2,0,0,0,0,0\n')
+			file1.write('1,0,4,0,0,0,0,0,0\n')
+		file1.close()
+		#file_handler_internal=FileHandler("valid_file.csv","r")
+		self.input_file=FileHandlerCSV("valid_file.csv","r")
+		self.file_invalid = "invalid_file.csv"
+		self.invalid_input_file=FileHandlerCSV("invalid_file.csv","r")
+		
+		self.file_invalid_expected = "invalid_file_new.csv"
+		self.expected_mode = "r"
+	
+	def test_CSV_file_retirns_the_content_in_a_matrix(self):
+		expected_result=[[4,0,0,0,0,0,8,0,5]\
+						,[0,3,0,0,0,0,0,0,0]\
+						,[0,0,0,7,0,0,0,0,0]\
+						,[0,2,0,0,0,0,0,6,0]\
+						,[0,0,0,0,8,0,4,0,0]\
+						,[0,0,0,0,1,0,0,0,0]\
+						,[0,0,0,6,0,3,0,7,0]\
+						,[5,0,0,2,0,0,0,0,0]\
+						,[1,0,4,0,0,0,0,0,0]]
+		self.assertEqual(expected_result, self.input_file.import_file())
+		
+	def test_CSV_file_returns_false_if_the_matrix_has_no_valid_format(self):
+		expected_result=False
+		self.assertEqual(expected_result, self.invalid_input_file.import_file())
+
+	def test_CSV_file_create_a_file_if_no_exist(self):
+		inexistent_input_file=FileHandlerCSV(self.file_invalid_expected,"r")
+		expected_result=False
+		self.assertEqual(expected_result, inexistent_input_file.import_file())
+		
+
+
+if __name__ == "__main__":
+	unittest.main()
